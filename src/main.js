824e32e7cb0812164b17f1d0eae101824bdccc82
cjhappTNG <94829904+cjhappTNG@users.noreply.github.com>
2026-07-24 13:53:31 +0200

    personalise

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Wolgang Wasserträger",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Failing runs"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Github actions",
     // What do you want to learn in this workshop?
     expectations: ["what you are talking about"],
   },
